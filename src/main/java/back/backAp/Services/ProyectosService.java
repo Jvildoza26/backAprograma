217a1b163839e9bd4eacf33431cb689f6f5d773f
Jvildoza26 <jonathanvildoza26@gmail.com>
2022-08-30 15:43:09 -0300

    Servicios

---
+++ src/main/java/back/backAp/Services/ProyectosService.java
@@ -0,0 +1,29 @@
+package back.backAp.Services;
+
+import back.backAp.Entity.Proyectos;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class ProyectosService {
+    private final ProyectosRepo proyectosRepo;
+
+    @Autowired
+    public ProyectosService(ProyectosRepo proyectosRepo) {
+        this.proyectosRepo = proyectosRepo;
+    }
+    public Proyectos addProyectos(Proyectos proyectos){
+        return proyectosRepo.save(proyectos);
+    }
+    public List<Proyectos> buscarProyectos(){
+        return proyectosRepo.findAll();
+    }
+    public Proyectos editarProyectos(Proyectos proyectos){
+        return proyectosRepo.save(proyectos);
+    }
+    public void borrarProyectos(Long id){proyectosRepo.deleteById(id);}
+}
---
+++ src/main/java/back/backAp/Services/SkillsService.java
@@ -0,0 +1,29 @@
+package back.backAp.Services;
+
+import back.backAp.Entity.Skills;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class SkillsService {
+    private final SkillsRepo skillsRepo;
+
+    @Autowired
+    public SkillsService(SkillsRepo skillsRepo) {
+        this.skillsRepo = skillsRepo;
+    }
+    public Skills addSkills(Skills skills){
+        return skillsRepo.save(skills);
+    }
+    public List<Skills> buscarSkills(){
+        return skillsRepo.findAll();
+    }
+    public Skills editarSkills(Skills skills){
+        return skillsRepo.save(skills);
+    }
+    public void borrarSkills(Long id){skillsRepo.deleteById(id);}
+}
